fb0a556e2f5b5918bdc708bc77cb7751df841c86
Angel Ocampo <angelocampo262@gmail.com>
2026-07-13 10:17:53 -0600

    feat: iconos y configuracion PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      manifest: {
+        name: 'Inventory Control System',
+        short_name: 'PapeControl',
+        description: 'Sistema Inteligente de Control de Inventarios',
+        theme_color: '#0d0f14',
+        background_color: '#0d0f14',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
-  base: '/inventory-control-front/'
+  base: '/inventory-control-front/',
 })
